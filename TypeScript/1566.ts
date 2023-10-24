803e110d4aaea5642dd866fc3d4676599ea25158
Aleksey <AlekseyVY@gmail.com>
2023-10-24 22:18:08 +0300

    1544

---
+++ TypeScript/1566.ts
@@ -0,0 +1,14 @@
+// Problem: https://leetcode.com/problems/make-the-string-great/
+
+function makeGood(s: string): string {
+  const stack: string[] = [];
+  for(let i = 0; i < s.length; i++) {
+      const char = s[i];
+      if(stack.length > 0 && stack[stack.length - 1].toLowerCase() === char.toLowerCase() && stack[stack.length - 1] !== char) {
+          stack.pop()
+      } else {
+          stack.push(char);
+      }
+  }
+  return stack.join('');
+};
