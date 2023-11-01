0fa3241125b6d56727f370d436dca8fe2eadac49
Aleksey <AlekseyVY@gmail.com>
2023-11-02 01:22:33 +0300

    2723

---
+++ TypeScript/2723.ts
@@ -0,0 +1,8 @@
+// Problem: https://leetcode.com/problems/add-two-promises/description/
+
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+	const [p1, p2] = await Promise.all([promise1, promise2]);
+    return p1 + p2;
+};
